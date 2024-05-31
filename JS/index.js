e948c834ac226345926c827c0564f8b307f03780
Oltinbek2010 <oltinbek04@gmail.com>
2024-05-31 14:55:04 +0300

    commit

---
+++ JS/index.js
@@ -0,0 +1,122 @@
+
+/* 1-masala
+let a = +prompt ("Kvadratning tomonini kiriting");
+let P = 4 * a;
+console.log(P);
+*/
+
+ /* 2-masala
+ let a = +prompt ("Kvadratning tomonini kiriting");
+ let S = a * a;
+ console.log(S);
+ */
+
+ /* 3-masala
+ let a = +prompt ("To'g'ri to'rtburchakning enini kiriting");
+ let b = +prompt ("To'g'ri to'rtburchakning bo'yini kiriting");
+ let S = a * b;
+ let P = 2 * (a + b);
+ console.log(P,S);
+ */
+
+ /* 4-masala
+
+ let d = +prompt ("Aylananing diametrini kiriting");
+ let L = Math.PI * d;
+ console.log(L); 
+ */
+
+ /* 5-masala
+
+ let a = +prompt ("Kubning yon tomonini kiriting");
+ let V = a * a * a;
+ let S = 6 * a * a;
+ console.log(V, S);
+ */
+
+ /* 6-masala
+
+ let a = +prompt ("Paralelepipedning enini kiriting");
+ let b = +prompt ("Paralelepipedning bo'yini kiriting");
+ let c = +prompt ("Paralelepipedning uzunligini kiriting");
+ let V = a * b * c;
+ let S = 2 * (a * b + b * c + a * c);
+ console.log(V, S);
+ */
+
+ /* 7-masala  
+
+ let R = +prompt ("Doiraning radiusini kiriting");
+let L = 2 * Math.PI * R;
+let S = Math.PI * R * R;
+console.log(L, S);
+*/
+
+/* 8-masala
+
+let a = +prompt ("Birinchi sonni kiriting");
+let b = +prompt ("Ikkinchi sonni kiriting");
+let c = (a + b) / 2;
+console.log(c);
+*/
+
+/* 9-masala
+
+let R1 = +prompt ("Birinchi aylananing radiusini kiriting");
+let R2 = +prompt ("Ikkinchi aylananing radiusini kiriting");
+let S1 = Math.PI * R1;
+let S2 = Math.PI * R2;
+let S3 = Math.PI * (R1 - R2);
+console.log(S3); 
+*/
+
+/* 10-masala
+
+
+let Tf = +prompt ("Selsiyni kiriting");
+let Tc = (Tf - 32) * 5 / 9;
+console.log(Tc);
+*/
+
+/* 11-masala
+
+let L = +prompt ("Uzunlikni kiriting");
+let M = L / 100;
+*/
+
+/* 12-masala
+
+let M = +prompt ("Og'irlikni kiriting");
+let T = M / 1000;
+*/
+
+/* 13-masala 
+
+let B = +prompt ("Hajmni kiriting");
+let Kb = B / 1024;
+*/
+
+/* 14-masala
+
+let A = +prompt ("Sonni kiriting");
+let B = +prompt ("Sonni kiriting");
+let R = A / B;
+console.log(R);
+*/
+
+/* 15-masala
+
+let A = +prompt ("Sonni kiriting");
+let B = +prompt ("Sonni kiriting");
+let R = A / B;
+let Jr = R * B;
+console.log(B);
+*/
+
+  /* 16-masala
+
+let n = +prompt ("Ikki xonali son kiriting");
+let O = n % 10;
+let B = n / 10;
+console.log(O, B);
+*/
